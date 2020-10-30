edc7ed98585dd3ef797933ac2cf5f0afdc0b19fd
Stjepan Banek <stjepan.banek@infinum.hr>
2020-10-30 11:09:09 +0100

    Add todo

--- core/src/main/java/com/infinum/jsonapix/core/Core.kt
+++ core/src/main/java/com/infinum/jsonapix/core/Core.kt
@@ -1,8 +1,10 @@
 package com.infinum.jsonapix.core
 
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-
+// TODO Find a way to make functions that will parse the annotated classes and annotated classes only. Keep commented section as a reference
 //inline fun <reified T> T.toJsonApiString(type: String): String {
 //    return Json.encodeToString(this)
 //}
@@ -10,8 +12,4 @@ import kotlinx.serialization.json.Json
 //inline fun <reified T> String.decodeFromJsonApi(): T {
 //    return Json.decodeFromString(this)
 //    //return Json.decodeFromString<JsonApiWrapper<T>>(this).data
-//}
-//
-//inline fun <reified T: JsonApiSerializable> T.getJsonApiSerializer(): JsonApiSerializer<T> {
-//    return JsonApiSerializer(serializer())
 //}
