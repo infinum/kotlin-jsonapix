1dbd1abc400df816403d5422abae63f8ee54ca86
Stjepan Banek <stjepan.banek@infinum.hr>
2020-11-12 13:37:53 +0100

    Remove redundant code

--- sample/src/main/java/com/infinum/jsonapix/MainActivity.kt
+++ sample/src/main/java/com/infinum/jsonapix/MainActivity.kt
@@ -3,9 +3,6 @@ package com.infinum.jsonapix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.serialization.*
-import kotlinx.serialization.json.Json
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,15 +19,6 @@ class MainActivity : AppCompatActivity() {
        textDecoded.text = jsonApiString.decodeJsonApiString<Person>().name
     }
 }
-
-@Serializable
-sealed class Parent {
-    abstract val name: String
-}
-
-
-@Serializable
-class Child(override val name: String, val age: Int) : Parent()
 
 
 
