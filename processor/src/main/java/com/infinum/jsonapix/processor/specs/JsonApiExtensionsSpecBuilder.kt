2e2d782b51732f05e7fe1a99737f53b87b8a1886
Stjepan Banek <stjepan.banek@infinum.hr>
2021-02-26 09:52:47 +0100

    Clean JsonApiExtensionsSpecBuilder

--- processor/src/main/java/com/infinum/jsonapix/processor/specs/JsonApiExtensionsSpecBuilder.kt
+++ processor/src/main/java/com/infinum/jsonapix/processor/specs/JsonApiExtensionsSpecBuilder.kt
@@ -2,6 +2,7 @@ package com.infinum.jsonapix.processor.specs
 
 import com.infinum.jsonapix.core.JsonApiWrapper
 import com.infinum.jsonapix.core.discriminators.JsonApiDiscriminator
+import com.infinum.jsonapix.core.discriminators.TypeExtractor
 import com.infinum.jsonapix.core.resources.ResourceObject
 import com.infinum.jsonapix.processor.ClassInfo
 import com.squareup.kotlinpoet.AnnotationSpec
@@ -14,6 +15,7 @@ import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeVariableName
 import com.squareup.kotlinpoet.asClassName
+import com.squareup.kotlinpoet.asTypeName
 import kotlinx.serialization.PolymorphicSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
@@ -21,33 +23,41 @@ import kotlinx.serialization.modules.SerializersModule
 internal class JsonApiExtensionsSpecBuilder {
 
     companion object {
-        private const val EXTENSIONS_PACKAGE = "com.infinum.jsonapix"
-        private const val EXTENSIONS_FILE_NAME = "JsonApiExtensions"
-        private const val CORE_DISCRIMINATORS_PACKAGE = "com.infinum.jsonapix.core.discriminators"
-        private const val WRAPPER_GETTER_FUNCTION_NAME = "toJsonApiWrapper"
-        private const val SERIALIZER_EXTENSION_NAME = "toJsonApiString"
-        private const val DESERIALIZER_EXTENSION_NAME = "decodeJsonApiString"
-        private const val DECODE_FROM_STRING_MEMBER_NAME = "decodeFromString"
-        private const val ENCODE_TO_STRING_MEMBER_NAME = "encodeToString"
-        private const val FORMAT_PROPERTY_NAME = "format"
-        private const val GENERIC_TYPE_VARIABLE_NAME = "T"
-        private const val INJECT_CLASS_DISCRIMINATOR_FUNCTION_NAME = "injectClassDiscriminator"
-        private const val EXTRACT_CLASS_DISCRIMINATOR_FUNCTION_NAME = "extractClassDiscriminator"
-        private const val FIND_TYPE_FUNCTION_NAME = "findType"
-        private const val SERIALIZERS_MODULE_PROPERTY_NAME = "jsonApiSerializerModule"
-        private const val KOTLINX_SERIALIZATION_PACKAGE = "kotlinx.serialization"
-        private const val KOTLINX_SERIALIZATION_MODULES_PACKAGE = "kotlinx.serialization.modules"
-        private const val POLYMORPHIC_FUNCTION_NAME = "polymorphic"
-        private const val SUBCLASS_FUNCTION_NAME = "subclass"
-        private const val CLASS_DISCRIMINATOR = "#class"
-        private const val ENCODE_DEFAULTS_STATEMENT = "encodeDefaults = true"
+        private const val PACKAGE_EXTENSIONS = "com.infinum.jsonapix"
+        private const val PACKAGE_KOTLINX_SERIALIZATION = "kotlinx.serialization"
+        private const val PACKAGE_KOTLINX_SERIALIZATION_MODULES = "kotlinx.serialization.modules"
+        private const val PACKAGE_CORE_DISCRIMINATORS = "com.infinum.jsonapix.core.discriminators"
+        private const val PACKAGE_KOTLINX_SERIALIZATION_JSON = "kotlinx.serialization.json"
+        private const val PACKAGE_TYPE_EXTRACTOR =
+            "com.infinum.jsonapix.core.discriminators.TypeExtractor"
 
-        private val JSON_API_WRAPPER_IMPORTS = arrayOf(
+        private const val FILE_NAME_EXTENSIONS = "JsonApiExtensions"
+
+        private const val MEMBER_WRAPPER_GETTER = "toJsonApiWrapper"
+        private const val MEMBER_SERIALIZER = "toJsonApiString"
+        private const val MEMBER_DESERIALIZER = "decodeJsonApiString"
+        private const val MEMBER_DECODE_FROM_STRING = "decodeFromString"
+        private const val MEMBER_ENCODE_TO_STRING = "encodeToString"
+        private const val MEMBER_FORMAT = "format"
+        private const val MEMBER_GENERIC_TYPE_VARIABLE = "T"
+        private const val MEMBER_FIND_TYPE = "findType"
+        private const val MEMBER_PARSE_TO_JSON_ELEMENT = "parseToJsonElement"
+        private const val MEMBER_SERIALIZERS_MODULE = "jsonApiSerializerModule"
+        private const val MEMBER_POLYMORPHIC = "polymorphic"
+        private const val MEMBER_SUBCLASS = "subclass"
+        private const val MEMBER_JSON_OBJECT = "jsonObject"
+
+        private const val CLASS_DISCRIMINATOR = "#class"
+        private const val KEY_DATA = "data"
+
+        private const val STATEMENT_ENCODE_DEFAULTS = "encodeDefaults = true"
+
+        private val IMPORTS_JSON_API_WRAPPER = arrayOf(
             "core.JsonApiWrapper",
             "core.resources.ResourceObject"
         )
 
-        private val KOTLINX_IMPORTS = arrayOf(
+        private val IMPORTS_KOTLINX = arrayOf(
             "json.Json",
             "json.jsonObject",
             "encodeToString",
@@ -55,13 +65,13 @@ internal class JsonApiExtensionsSpecBuilder {
             "PolymorphicSerializer"
         )
 
-        private val KOTLINX_MODULES_IMPORTS = arrayOf(
+        private val IMPORTS_KOTLINX_MODULES = arrayOf(
             "polymorphic",
             "subclass",
             "SerializersModule"
         )
 
-        private val CORE_EXTENSIONS_IMPORTS = arrayOf(
+        private val IMPORTS_CORE_EXTENSIONS = arrayOf(
             "JsonApiDiscriminator",
             "TypeExtractor"
         )
@@ -74,24 +84,38 @@ internal class JsonApiExtensionsSpecBuilder {
     }
 
     private fun deserializeFunSpec(): FunSpec {
-        val typeVariableName = TypeVariableName.invoke(GENERIC_TYPE_VARIABLE_NAME)
-        val decodeMember = MemberName(KOTLINX_SERIALIZATION_PACKAGE, DECODE_FROM_STRING_MEMBER_NAME)
-        val formatMember = MemberName(EXTENSIONS_PACKAGE, FORMAT_PROPERTY_NAME)
-        return FunSpec.builder(DESERIALIZER_EXTENSION_NAME)
+        val typeVariableName = TypeVariableName.invoke(MEMBER_GENERIC_TYPE_VARIABLE)
+        val decodeMember = MemberName(PACKAGE_KOTLINX_SERIALIZATION, MEMBER_DECODE_FROM_STRING)
+        val formatMember = MemberName(PACKAGE_EXTENSIONS, MEMBER_FORMAT)
+        val findTypeMember = MemberName(PACKAGE_TYPE_EXTRACTOR, MEMBER_FIND_TYPE)
+        val jsonObjectMember = MemberName(PACKAGE_KOTLINX_SERIALIZATION_JSON, MEMBER_JSON_OBJECT)
+        return FunSpec.builder(MEMBER_DESERIALIZER)
             .receiver(String::class)
             .addModifiers(KModifier.INLINE)
             .addTypeVariable(typeVariableName.copy(reified = true))
             .returns(typeVariableName.copy(nullable = true))
-            .addStatement("val type = %L.findType(Json.parseToJsonElement(this).jsonObject[\"data\"]!!)", "TypeExtractor")
+            .addStatement(
+                "val type = %T.%M(%T.%L(this).%M[%S]!!)",
+                TypeExtractor::class.asTypeName(),
+                findTypeMember,
+                Json::class.asTypeName(),
+                MEMBER_PARSE_TO_JSON_ELEMENT,
+                jsonObjectMember,
+                KEY_DATA
+            )
             .addStatement("val discriminator = %T(type)", JsonApiDiscriminator::class)
-            .addStatement("val jsonElement = Json.parseToJsonElement(this)")
+            .addStatement(
+                "val jsonElement = %T.%L(this)",
+                Json::class.asClassName(),
+                MEMBER_PARSE_TO_JSON_ELEMENT
+            )
             .addStatement("val jsonStringWithDiscriminator = discriminator.inject(jsonElement).toString()")
             .addStatement(
                 "return %M.%M<%T<%T>>(jsonStringWithDiscriminator).data?.attributes",
                 formatMember,
                 decodeMember,
                 JsonApiWrapper::class,
-                typeVariableName,
+                typeVariableName
             )
             .build()
     }
@@ -100,12 +124,12 @@ internal class JsonApiExtensionsSpecBuilder {
         val formatCodeBuilder = CodeBlock.builder()
             .addStatement("%T {", Json::class)
             .indent()
-            .addStatement(ENCODE_DEFAULTS_STATEMENT)
+            .addStatement(STATEMENT_ENCODE_DEFAULTS)
             .addStatement("classDiscriminator = %S", CLASS_DISCRIMINATOR)
-            .addStatement("serializersModule = %L", SERIALIZERS_MODULE_PROPERTY_NAME)
+            .addStatement("serializersModule = %L", MEMBER_SERIALIZERS_MODULE)
             .unindent()
             .addStatement("}")
-        return PropertySpec.builder(FORMAT_PROPERTY_NAME, Json::class)
+        return PropertySpec.builder(MEMBER_FORMAT, Json::class)
             .initializer(formatCodeBuilder.build())
             .build()
     }
@@ -113,12 +137,12 @@ internal class JsonApiExtensionsSpecBuilder {
     private fun jsonApiWrapperSerializerPropertySpec(): PropertySpec {
         val codeBlockBuilder = CodeBlock.builder()
         val polymorpicMember = MemberName(
-            KOTLINX_SERIALIZATION_MODULES_PACKAGE,
-            POLYMORPHIC_FUNCTION_NAME
+            PACKAGE_KOTLINX_SERIALIZATION_MODULES,
+            MEMBER_POLYMORPHIC
         )
         val subclassMember = MemberName(
-            KOTLINX_SERIALIZATION_MODULES_PACKAGE,
-            SUBCLASS_FUNCTION_NAME
+            PACKAGE_KOTLINX_SERIALIZATION_MODULES,
+            MEMBER_SUBCLASS
         )
         codeBlockBuilder.addStatement("%T {", SerializersModule::class)
         codeBlockBuilder.indent()
@@ -143,69 +167,76 @@ internal class JsonApiExtensionsSpecBuilder {
         codeBlockBuilder.unindent().addStatement("}")
         codeBlockBuilder.unindent().addStatement("}")
 
-        return PropertySpec.builder(SERIALIZERS_MODULE_PROPERTY_NAME, SerializersModule::class)
+        return PropertySpec.builder(MEMBER_SERIALIZERS_MODULE, SerializersModule::class)
             .initializer(codeBlockBuilder.build()).build()
     }
 
+    private fun serializeFunSpec(originalClass: ClassName): FunSpec {
+        val polymorphicSerializerClass = PolymorphicSerializer::class.asClassName()
+        val jsonApiWrapperClass = JsonApiWrapper::class.asClassName()
+        val formatMember = MemberName(PACKAGE_EXTENSIONS, MEMBER_FORMAT)
+        val encodeMember =
+            MemberName(PACKAGE_KOTLINX_SERIALIZATION, MEMBER_ENCODE_TO_STRING)
+        val jsonApiWrapperMember = MemberName(PACKAGE_EXTENSIONS, MEMBER_WRAPPER_GETTER)
+        return FunSpec.builder(MEMBER_SERIALIZER)
+            .receiver(originalClass)
+            .returns(String::class)
+            .addStatement("val jsonWrapper = this.%M()", jsonApiWrapperMember)
+            .addStatement(
+                "val discriminator = %T(jsonWrapper.data.type)",
+                JsonApiDiscriminator::class.asClassName()
+            )
+            .addStatement(
+                "val jsonString = %M.%M(%T(%T::class), jsonWrapper)",
+                formatMember,
+                encodeMember,
+                polymorphicSerializerClass,
+                jsonApiWrapperClass
+            )
+            .addStatement("return discriminator.extract(Json.parseToJsonElement(jsonString)).toString()")
+            .build()
+    }
+
+    private fun wrapperFunSpec(originalClass: ClassName, wrapperClass: ClassName): FunSpec =
+        FunSpec.builder(MEMBER_WRAPPER_GETTER)
+            .receiver(originalClass)
+            .returns(wrapperClass)
+            .addStatement(
+                "return %T(%T_%T(this))",
+                wrapperClass,
+                ResourceObject::class.asClassName(),
+                originalClass
+            )
+            .build()
+
     @SuppressWarnings("SpreadOperator")
     fun build(): FileSpec {
-        val fileSpec = FileSpec.builder(EXTENSIONS_PACKAGE, EXTENSIONS_FILE_NAME)
+        val fileSpec = FileSpec.builder(PACKAGE_EXTENSIONS, FILE_NAME_EXTENSIONS)
         fileSpec.addAnnotation(
-            AnnotationSpec.builder(JvmName::class).addMember("%S", EXTENSIONS_FILE_NAME)
+            AnnotationSpec.builder(JvmName::class).addMember("%S", FILE_NAME_EXTENSIONS)
                 .useSiteTarget(AnnotationSpec.UseSiteTarget.FILE).build()
         )
 
         fileSpec.addImport(
-            KOTLINX_SERIALIZATION_PACKAGE,
-            *KOTLINX_IMPORTS
+            PACKAGE_KOTLINX_SERIALIZATION,
+            *IMPORTS_KOTLINX
         )
 
         fileSpec.addImport(
-            CORE_DISCRIMINATORS_PACKAGE,
-            *CORE_EXTENSIONS_IMPORTS
+            PACKAGE_CORE_DISCRIMINATORS,
+            *IMPORTS_CORE_EXTENSIONS
         )
 
         fileSpec.addImport(
-            KOTLINX_SERIALIZATION_MODULES_PACKAGE,
-            *KOTLINX_MODULES_IMPORTS
+            PACKAGE_KOTLINX_SERIALIZATION_MODULES,
+            *IMPORTS_KOTLINX_MODULES
         )
 
-        fileSpec.addImport(EXTENSIONS_PACKAGE, *JSON_API_WRAPPER_IMPORTS)
+        fileSpec.addImport(PACKAGE_EXTENSIONS, *IMPORTS_JSON_API_WRAPPER)
 
         specsMap.entries.forEach {
-            fileSpec.addFunction(
-                FunSpec.builder(WRAPPER_GETTER_FUNCTION_NAME)
-                    .receiver(it.key)
-                    .returns(it.value.jsonWrapperClassName)
-                    .addStatement(
-                        "return %T(%T_%T(this))",
-                        it.value.jsonWrapperClassName,
-                        ResourceObject::class.asClassName(),
-                        it.key
-                    )
-                    .build()
-            )
-
-            val polymorphicSerializerClass = PolymorphicSerializer::class.asClassName()
-            val jsonApiWrapperClass = JsonApiWrapper::class.asClassName()
-            val formatMember = MemberName(EXTENSIONS_PACKAGE, FORMAT_PROPERTY_NAME)
-            val encodeMember = MemberName(KOTLINX_SERIALIZATION_PACKAGE, ENCODE_TO_STRING_MEMBER_NAME)
-            fileSpec.addFunction(
-                FunSpec.builder(SERIALIZER_EXTENSION_NAME)
-                    .receiver(it.key)
-                    .returns(String::class)
-                    .addStatement("val type = this.toJsonApiWrapper()")
-                    .addStatement("val discriminator = JsonApiDiscriminator(type.data.type)")
-                    .addStatement(
-                        "val jsonString = %M.%M(%T(%T::class), type)",
-                        formatMember,
-                        encodeMember,
-                        polymorphicSerializerClass,
-                        jsonApiWrapperClass
-                    )
-                    .addStatement("return discriminator.extract(Json.parseToJsonElement(jsonString)).toString()")
-                    .build()
-            )
+            fileSpec.addFunction(wrapperFunSpec(it.key, it.value.jsonWrapperClassName))
+            fileSpec.addFunction(serializeFunSpec(it.key))
         }
 
         fileSpec.addProperty(jsonApiWrapperSerializerPropertySpec())
