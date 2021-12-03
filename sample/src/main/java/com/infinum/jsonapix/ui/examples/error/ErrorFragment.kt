308e9703d5073e95a677e98fbf225f30a82f9ef7
Stjepan Banek <stjepan.banek@infinum.hr>
2021-12-03 12:54:01 +0100

    Add error fetching example

--- sample/src/main/java/com/infinum/jsonapix/ui/examples/error/ErrorFragment.kt
+++ sample/src/main/java/com/infinum/jsonapix/ui/examples/error/ErrorFragment.kt
@@ -1,11 +1,15 @@
 package com.infinum.jsonapix.ui.examples.error
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.infinum.jsonapix.R
 import com.infinum.jsonapix.databinding.FragmentErrorBinding
 import com.infinum.jsonapix.extensions.viewBinding
 import com.infinum.jsonapix.ui.shared.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ErrorFragment : BaseFragment<ErrorState, ErrorEvent>() {
 
     companion object {
@@ -21,4 +25,11 @@ class ErrorFragment : BaseFragment<ErrorState, ErrorEvent>() {
     override fun handleState(state: ErrorState) = Unit
 
     override fun handleEvent(event: ErrorEvent) = Unit
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.downloadButton.setOnClickListener {
+            viewModel.fetchError()
+        }
+    }
 }
--- sample/src/main/java/com/infinum/jsonapix/ui/examples/error/ErrorViewModel.kt
+++ sample/src/main/java/com/infinum/jsonapix/ui/examples/error/ErrorViewModel.kt
@@ -1,8 +1,31 @@
 package com.infinum.jsonapix.ui.examples.error
 
+import com.infinum.jsonapix.data.api.SampleApiService
+import com.infinum.jsonapix.data.assets.JsonAssetReader
 import com.infinum.jsonapix.ui.shared.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import retrofit2.HttpException
 
 @HiltViewModel
-class ErrorViewModel @Inject constructor() : BaseViewModel<ErrorState, ErrorEvent>()
+class ErrorViewModel @Inject constructor(
+    private val sampleApiService: SampleApiService,
+    private val jsonAssetReader: JsonAssetReader
+) : BaseViewModel<ErrorState, ErrorEvent>() {
+
+    fun fetchError() {
+        launch {
+            showLoading()
+            val bodyString = io { jsonAssetReader.readJsonAsset("responses/error.json") }
+            try {
+                val person = io { sampleApiService.fetchError() }
+            } catch (e: HttpException) {
+                val errorBody = e.response()?.errorBody()?.string()
+                showError(errorBody!!)
+            }
+
+            hideLoading()
+        }
+    }
+}
+
