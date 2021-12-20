0f92f9720f52ce9ad443610286faef8d2d2db680
Stjepan Banek <stjepan.banek@infinum.hr>
2021-12-20 10:17:04 +0100

    Add default links to wrappers

--- core/src/main/java/com/infinum/jsonapix/core/JsonApiX.kt
+++ core/src/main/java/com/infinum/jsonapix/core/JsonApiX.kt
@@ -1,12 +1,14 @@
 package com.infinum.jsonapix.core
 
 import com.infinum.jsonapix.core.resources.Error
+import com.infinum.jsonapix.core.resources.Links
 import com.infinum.jsonapix.core.resources.ResourceObject
 
 interface JsonApiX<out Model> {
     val data: ResourceObject<Model>?
     val included: List<ResourceObject<*>>?
     val errors: List<Error>?
+    val links: Links?
 
     val original: Model
 }
--- core/src/main/java/com/infinum/jsonapix/core/JsonApiXList.kt
+++ core/src/main/java/com/infinum/jsonapix/core/JsonApiXList.kt
@@ -1,12 +1,14 @@
 package com.infinum.jsonapix.core
 
 import com.infinum.jsonapix.core.resources.Error
+import com.infinum.jsonapix.core.resources.Links
 import com.infinum.jsonapix.core.resources.ResourceObject
 
 interface JsonApiXList<out Model> {
     val data: List<ResourceObject<Model>>?
     val included: List<ResourceObject<*>>?
     val errors: List<Error>?
+    val links: Links?
 
     val original: List<Model>
 }
