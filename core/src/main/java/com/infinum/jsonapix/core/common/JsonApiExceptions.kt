c9484e6dc943e7657ddcd030febf1c428ceac660
gobbletsit <nikola.gobovic@infinum.hr>
2022-04-01 18:55:58 +0200

    Create JsonApiXMissingArgumentException and use it for ROSB originalFunSpec nullability and code refactor

--- core/src/main/java/com/infinum/jsonapix/core/common/JsonApiExceptions.kt
+++ core/src/main/java/com/infinum/jsonapix/core/common/JsonApiExceptions.kt
@@ -1,1 +1,7 @@
 package com.infinum.jsonapix.core.common
+
+import java.lang.IllegalArgumentException
+
+class JsonApiXMissingArgumentException : IllegalArgumentException() {
+    override val message: String = "Missing attribute"
+}
