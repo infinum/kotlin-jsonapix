5e0715758c306a79d54f0356e41b76af1d4ccb30
gobbletsit <nikola.gobovic@infinum.hr>
2022-09-27 14:38:02 +0200

    Remove jsonapixlint lib android module as its not needed, modify jsonapixdetectortest

--- lint/src/test/java/com/infinum/jsonapix/lint/JsonApiXCodeDetectorTest.kt
+++ lint/src/test/java/com/infinum/jsonapix/lint/JsonApiXCodeDetectorTest.kt
@@ -1,20 +1,25 @@
 package com.infinum.jsonapix.lint
 
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest
 import com.android.tools.lint.checks.infrastructure.TestFile
 import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Issue
 import org.junit.Test
 
 @Suppress("UnstableApiUsage")
-class JsonApiXCodeDetectorTest {
+class JsonApiXCodeDetectorTest: LintDetectorTest() {
 
     object Stubs {
         val TEST: TestFile = kotlin(
             """
-                    package com.infinum.jsonapix.lint;
-                    import com.android.annotations.concurrency.AnyThread
+                    package com.infinum.jsonapix.lint
+
+                    import com.infinum.jsonapix.annotations.JsonApiX
+
                     @JsonApiX
-                    class TestClass1 (val kurac: String)
+                    class TestClass1 (val someValue: String)
                     """
         ).indented()
     }
@@ -33,4 +38,8 @@ class JsonApiXCodeDetectorTest {
                     """
             )
     }
+
+    override fun getDetector(): Detector = JsonApiXCodeDetector()
+
+    override fun getIssues(): MutableList<Issue> = mutableListOf(JsonApiXCodeDetector.ANNOTATION_USAGE_ISSUE)
 }
