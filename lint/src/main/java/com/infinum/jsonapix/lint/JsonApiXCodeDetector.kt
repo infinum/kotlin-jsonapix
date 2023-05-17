cbcda90d2462cf80dbf73d7cdea01bafcd8302bc
Ahmed Ali <ahmedaliaay@gmail.com>
2023-05-17 12:34:48 +0300

    Add lint rule to make sure nullable data has default value for all attrs

--- lint/src/main/java/com/infinum/jsonapix/lint/JsonApiXCodeDetector.kt
+++ lint/src/main/java/com/infinum/jsonapix/lint/JsonApiXCodeDetector.kt
@@ -11,6 +11,7 @@ import com.android.tools.lint.detector.api.Severity
 import org.jetbrains.uast.UAnnotated
 import org.jetbrains.uast.UAnnotation
 import org.jetbrains.uast.UElement
+import org.jetbrains.uast.getContainingUClass
 import org.jetbrains.uast.getParentOfType
 import java.util.EnumSet
 
@@ -21,26 +22,47 @@ private const val JSON_API_X_SIMPLE_NAME = "JsonApiX"
 private const val JSON_API_X_META_SIMPLE_NAME = "JsonApiXMeta"
 private const val JSON_API_X_LINKS_SIMPLE_NAME = "JsonApiXLinks"
 private const val SERIALIZABLE = "kotlinx.serialization.Serializable"
+private const val JSON_API_X_IS_NULLABLE_ATTR = "isNullable"
 
 @Suppress("UnstableApiUsage")
 class JsonApiXCodeDetector : Detector(), Detector.UastScanner {
 
-    override fun getApplicableUastTypes(): List<Class<out UElement>> = listOf(UAnnotation::class.java)
+    override fun getApplicableUastTypes(): List<Class<out UElement>> =
+        listOf(UAnnotation::class.java)
 
     override fun createUastHandler(context: JavaContext): UElementHandler {
         return object : UElementHandler() {
 
             override fun visitAnnotation(node: UAnnotation) {
                 when (node.qualifiedName) {
-                    JSON_API_X_QUALIFIED_NAME -> validateAnnotationUsage(context, node, JSON_API_X_SIMPLE_NAME)
-                    JSON_API_X_META_QUALIFIED_NAME -> validateAnnotationUsage(context, node, JSON_API_X_META_SIMPLE_NAME)
-                    JSON_API_X_LINKS_QUALIFIED_NAME -> validateAnnotationUsage(context, node, JSON_API_X_LINKS_SIMPLE_NAME)
+                    JSON_API_X_QUALIFIED_NAME -> validateAnnotationUsage(
+                        context,
+                        node,
+                        JSON_API_X_SIMPLE_NAME
+                    )
+
+                    JSON_API_X_META_QUALIFIED_NAME -> validateAnnotationUsage(
+                        context,
+                        node,
+                        JSON_API_X_META_SIMPLE_NAME
+                    )
+
+                    JSON_API_X_LINKS_QUALIFIED_NAME -> validateAnnotationUsage(
+                        context,
+                        node,
+                        JSON_API_X_LINKS_SIMPLE_NAME
+                    )
                 }
             }
         }
     }
 
-    private fun validateAnnotationUsage(context: JavaContext, node: UAnnotation, annotationName: String) {
+    private fun validateAnnotationUsage(
+        context: JavaContext,
+        node: UAnnotation,
+        annotationName: String
+    ) {
+
         if (isInvalidAnnotationUsage(node)) {
             context.report(
                 createAnnotationUsageIssue(annotationName),
@@ -49,10 +71,40 @@ class JsonApiXCodeDetector : Detector(), Detector.UastScanner {
                 "$annotationName must be combined with @Serializable"
             )
         }
+
+        if (node.getContainingUClass()?.name == "EmptyData") {
+            context.log(
+                exception = null,
+                format = "$annotationName -> ${isPrimaryDataNullable(node)} -> ${
+                    isAllFieldsHasDefaultValues(node)
+                }"
+            )
+        }
+
+        if (annotationName == JSON_API_X_SIMPLE_NAME && isPrimaryDataNullable(node) && !isAllFieldsHasDefaultValues(
+                node
+            )
+        ) {
+            context.report(
+                createNullableDataDefaultValuesIssue(),
+                node,
+                context.getLocation(node),
+                "Nullable $JSON_API_X_SIMPLE_NAME  attributes should have default values"
+            )
+        }
     }
 
+
     private fun isInvalidAnnotationUsage(node: UAnnotation): Boolean =
-        node.getParentOfType<UAnnotated>()?.uAnnotations?.any { it.qualifiedName == SERIALIZABLE }?.not() ?: false
+        node.getParentOfType<UAnnotated>()?.uAnnotations?.any { it.qualifiedName == SERIALIZABLE }
+            ?.not() ?: false
+
+    private fun isPrimaryDataNullable(annotation: UAnnotation): Boolean =
+        annotation.attributeValues.firstOrNull { it.name == JSON_API_X_IS_NULLABLE_ATTR }
+            ?.evaluate() as? Boolean ?: false
+
+    private fun isAllFieldsHasDefaultValues(annotation: UAnnotation): Boolean =
+        annotation.getContainingUClass()?.fields?.all { it.uastInitializer != null } ?: false
 
     companion object {
 
@@ -72,11 +124,29 @@ class JsonApiXCodeDetector : Detector(), Detector.UastScanner {
                 )
             )
 
+        fun createNullableDataDefaultValuesIssue(): Issue =
+            Issue.create(
+                id = "IllegalNullable${JSON_API_X_SIMPLE_NAME}Annotation",
+                briefDescription = "Illegal use of isNullable attr in $JSON_API_X_SIMPLE_NAME annotation",
+                explanation = """
+                    When setting $JSON_API_X_IS_NULLABLE_ATTR in $JSON_API_X_SIMPLE_NAME to true, all fields must have default value in case 
+                    the returned data is null.
+                    """, // no need to .trimIndent(), lint does that automatically
+                category = Category.CUSTOM_LINT_CHECKS,
+                priority = 9,
+                severity = Severity.ERROR,
+                implementation = Implementation(
+                    JsonApiXCodeDetector::class.java,
+                    EnumSet.of(Scope.JAVA_FILE, Scope.TEST_SOURCES)
+                )
+            )
+
         @JvmField
         val annotationIssues: List<Issue> = listOf(
             createAnnotationUsageIssue(JSON_API_X_SIMPLE_NAME),
             createAnnotationUsageIssue(JSON_API_X_META_SIMPLE_NAME),
-            createAnnotationUsageIssue(JSON_API_X_LINKS_SIMPLE_NAME)
+            createAnnotationUsageIssue(JSON_API_X_LINKS_SIMPLE_NAME),
+            createNullableDataDefaultValuesIssue()
         )
     }
 }
