fe5cb349837514cc2bd4e0eddb5c18c21f26daec
Ahmed Ali <ahmedaliaay@gmail.com>
2023-05-17 18:07:48 +0300

    Add a Placement Strategy to JsonApiXMeta

--- annotations/src/main/java/com/infinum/jsonapix/annotations/JsonApiXMeta.kt
+++ annotations/src/main/java/com/infinum/jsonapix/annotations/JsonApiXMeta.kt
@@ -3,4 +3,11 @@ package com.infinum.jsonapix.annotations
 @MustBeDocumented
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class JsonApiXMeta(val type: String)
+annotation class JsonApiXMeta(
+    val type: String,
+    val placementStrategy: MetaPlacementStrategy = MetaPlacementStrategy.ROOT,
+)
+
+enum class MetaPlacementStrategy {
+    ROOT, DATA, RELATIONSHIPS
+}
