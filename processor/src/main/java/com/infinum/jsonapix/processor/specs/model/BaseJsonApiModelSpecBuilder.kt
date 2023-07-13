19565fdb79ce0f7a7392657ac389593789a962db
Ahmed Ali <ahmedaliaay@gmail.com>
2023-07-13 13:30:03 +0300

    Make getParams function fully customisable

--- processor/src/main/java/com/infinum/jsonapix/processor/specs/model/BaseJsonApiModelSpecBuilder.kt
+++ processor/src/main/java/com/infinum/jsonapix/processor/specs/model/BaseJsonApiModelSpecBuilder.kt
@@ -1,10 +1,6 @@
 package com.infinum.jsonapix.processor.specs.model
 
-import com.infinum.jsonapix.core.common.JsonApiConstants
 import com.infinum.jsonapix.core.common.JsonApiConstants.withName
-import com.infinum.jsonapix.core.resources.Error
-import com.infinum.jsonapix.core.resources.Links
-import com.infinum.jsonapix.core.resources.Meta
 import com.infinum.jsonapix.processor.LinksInfo
 import com.infinum.jsonapix.processor.MetaInfo
 import com.squareup.kotlinpoet.ClassName
@@ -12,18 +8,21 @@ import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.ParameterSpec
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.TypeSpec
-import com.squareup.kotlinpoet.asClassName
 
 internal abstract class BaseJsonApiModelSpecBuilder {
 
     abstract fun getClassSuffixName(): String
     abstract fun getRootClassName(rootType: ClassName): TypeName
+    abstract fun getParams(
+        className: ClassName,
+        isRootNullable: Boolean,
+        metaInfo: MetaInfo?,
+        linksInfo: LinksInfo?
+    ): List<ParameterSpec>
 
-    abstract fun getAdditionalParams(): List<ParameterSpec>
     fun build(
         className: ClassName,
         isRootNullable: Boolean,
@@ -31,7 +30,6 @@ internal abstract class BaseJsonApiModelSpecBuilder {
         linksInfo: LinksInfo?
     ): FileSpec {
         val generatedName = className.simpleName.withName(getClassSuffixName())
-
 
         val params = getParams(className, isRootNullable, metaInfo, linksInfo)
         val props = params.map { it.toPropSpec() }
@@ -51,38 +49,6 @@ internal abstract class BaseJsonApiModelSpecBuilder {
             .build()
     }
 
-    fun getParams(
-        className: ClassName,
-        isRootNullable: Boolean,
-        metaInfo: MetaInfo?,
-        linksInfo: LinksInfo?
-    ): List<ParameterSpec> {
-        return listOf(
-            *getAdditionalParams().toTypedArray(),
-            JsonApiConstants.Keys.DATA.asParam(getRootClassName(className), isRootNullable),
-            JsonApiConstants.Members.ROOT_LINKS.asParam(Links::class.asClassName(), true),
-            JsonApiConstants.Members.RESOURCE_OBJECT_LINKS.asParam(Links::class.asClassName(), true),
-            JsonApiConstants.Members.RELATIONSHIPS_LINKS.asParam(
-                Map::class.asClassName().parameterizedBy(
-                    String::class.asClassName(),
-                    Links::class.asClassName().copy(nullable = true),
-                ),
-                true,
-            ),
-
-            JsonApiConstants.Keys.ERRORS.asParam(List::class.asClassName().parameterizedBy(Error::class.asClassName()), true),
-
-            JsonApiConstants.Members.ROOT_META.asParam(metaInfo?.rootClassName ?: Meta::class.asClassName(), true),
-            JsonApiConstants.Members.RESOURCE_OBJECT_META.asParam(metaInfo?.resourceObjectClassName ?: Meta::class.asClassName(), true),
-            JsonApiConstants.Members.RELATIONSHIPS_META.asParam(
-                Map::class.asClassName().parameterizedBy(
-                    String::class.asClassName(),
-                    metaInfo?.relationshipsClassNAme?.copy(nullable = true) ?: Meta::class.asClassName().copy(nullable = true),
-                ),
-                true
-            ),
-        )
-    }
 
     protected fun String.asParam(className: TypeName, isNullable: Boolean): ParameterSpec {
         return ParameterSpec.builder(
--- processor/src/main/java/com/infinum/jsonapix/processor/specs/model/JsonApiModelSpecBuilder.kt
+++ processor/src/main/java/com/infinum/jsonapix/processor/specs/model/JsonApiModelSpecBuilder.kt
@@ -1,17 +1,46 @@
 package com.infinum.jsonapix.processor.specs.model
 
 import com.infinum.jsonapix.core.common.JsonApiConstants
+import com.infinum.jsonapix.core.resources.Error
+import com.infinum.jsonapix.core.resources.Links
+import com.infinum.jsonapix.core.resources.Meta
+import com.infinum.jsonapix.processor.LinksInfo
+import com.infinum.jsonapix.processor.MetaInfo
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.asClassName
 
 internal object JsonApiModelSpecBuilder : BaseJsonApiModelSpecBuilder() {
     override fun getClassSuffixName(): String = JsonApiConstants.Suffix.JSON_API_MODEL
     override fun getRootClassName(rootType: ClassName): ClassName = rootType
-    override fun getAdditionalParams(): List<ParameterSpec> {
+    override fun getParams(className: ClassName, isRootNullable: Boolean, metaInfo: MetaInfo?, linksInfo: LinksInfo?): List<ParameterSpec> {
         return listOf(
             JsonApiConstants.Keys.TYPE.asParam(String::class.asClassName(), true),
             JsonApiConstants.Keys.ID.asParam(String::class.asClassName(), true),
+            JsonApiConstants.Keys.DATA.asParam(getRootClassName(className), isRootNullable),
+            JsonApiConstants.Members.ROOT_LINKS.asParam(Links::class.asClassName(), true),
+            JsonApiConstants.Members.RESOURCE_OBJECT_LINKS.asParam(Links::class.asClassName(), true),
+            JsonApiConstants.Members.RELATIONSHIPS_LINKS.asParam(
+                Map::class.asClassName().parameterizedBy(
+                    String::class.asClassName(),
+                    Links::class.asClassName().copy(nullable = true),
+                ),
+                true,
+            ),
+
+            JsonApiConstants.Keys.ERRORS.asParam(List::class.asClassName().parameterizedBy(Error::class.asClassName()), true),
+
+            JsonApiConstants.Members.ROOT_META.asParam(metaInfo?.rootClassName ?: Meta::class.asClassName(), true),
+            JsonApiConstants.Members.RESOURCE_OBJECT_META.asParam(metaInfo?.resourceObjectClassName ?: Meta::class.asClassName(), true),
+            JsonApiConstants.Members.RELATIONSHIPS_META.asParam(
+                Map::class.asClassName().parameterizedBy(
+                    String::class.asClassName(),
+                    metaInfo?.relationshipsClassNAme?.copy(nullable = true) ?: Meta::class.asClassName().copy(nullable = true),
+                ),
+                true
+            ),
         )
     }
 }
+
