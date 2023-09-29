3074629d44cf165834f875fc3f994f7027b87a59
Ahmed Ali <ahmedaliaay@gmail.com>
2023-09-29 18:40:44 +0300

    Filter Rel meta and links from null values in XItem

--- processor/src/main/java/com/infinum/jsonapix/processor/specs/JsonApiXListSpecBuilder.kt
+++ processor/src/main/java/com/infinum/jsonapix/processor/specs/JsonApiXListSpecBuilder.kt
@@ -99,7 +99,7 @@ internal object JsonApiXListSpecBuilder : BaseJsonApiXSpecBuilder() {
             .addStatement("val items = data.map {")
             .addStatement("val original = it.original(included)")
             .addStatement(
-                "%T(%L,%L,%L,%L,%L?.mapValues{ it.value as? %T } )",
+                "%T(%L,%L,%L?.filterValues{ it != null },%L,%L?.filterValues{ it != null }?.mapValues{ it.value as? %T } )",
                 itemClassName,
                 "original",
                 "it.links",
