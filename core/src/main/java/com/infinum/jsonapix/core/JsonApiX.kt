c92cc98b54e1e83d26024da0d925a0674a3ca79e
Ahmed Ali <ahmedaliaay@gmail.com>
2023-10-17 17:38:57 +0300

    Write documentation to JsonApiX interface

--- core/src/main/java/com/infinum/jsonapix/core/JsonApiX.kt
+++ core/src/main/java/com/infinum/jsonapix/core/JsonApiX.kt
@@ -5,6 +5,15 @@ import com.infinum.jsonapix.core.resources.Links
 import com.infinum.jsonapix.core.resources.Meta
 import com.infinum.jsonapix.core.resources.ResourceObject
 
+
+/**
+ * The `JsonApiX` interface represents a generic JSON API response.
+ * It is designed to encapsulate the main components of a JSON API response,
+ * including the primary data, included resources, errors, links, and meta information.
+ *
+ * @param Data The type of the primary data object, typically a domain model.
+ * @param Model The type of the original response model, which may include additional fields not covered by this interface.
+ */
 interface JsonApiX<out Data, out Model> {
     val data: ResourceObject<Data>?
     val included: List<ResourceObject<*>>?
