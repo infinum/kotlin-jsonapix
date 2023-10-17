be4ad61e9bdc4f4fd20e01d29b83f255a783493b
Ahmed Ali <ahmedaliaay@gmail.com>
2023-10-17 17:41:38 +0300

    Make DefaultLinks a data class

--- core/src/main/java/com/infinum/jsonapix/core/resources/DefaultLinks.kt
+++ core/src/main/java/com/infinum/jsonapix/core/resources/DefaultLinks.kt
@@ -5,39 +5,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 @SerialName("links")
-class DefaultLinks(
+data class DefaultLinks(
     @SerialName("self") val self: String? = null,
     @SerialName("related") val related: String? = null,
     @SerialName("first") val first: String? = null,
     @SerialName("last") val last: String? = null,
     @SerialName("next") val next: String? = null,
     @SerialName("prev") val prev: String? = null
-) : Links {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as DefaultLinks
-
-        if (self != other.self) return false
-        if (related != other.related) return false
-        if (first != other.first) return false
-        if (last != other.last) return false
-        if (next != other.next) return false
-        return prev == other.prev
-    }
-
-    override fun hashCode(): Int {
-        var result = self?.hashCode() ?: 0
-        result = 31 * result + (related?.hashCode() ?: 0)
-        result = 31 * result + (first?.hashCode() ?: 0)
-        result = 31 * result + (last?.hashCode() ?: 0)
-        result = 31 * result + (next?.hashCode() ?: 0)
-        result = 31 * result + (prev?.hashCode() ?: 0)
-        return result
-    }
-
-    override fun toString(): String {
-        return "DefaultLinks(self=$self, related=$related, first=$first, last=$last, next=$next, prev=$prev)"
-    }
-}
+) : Links
