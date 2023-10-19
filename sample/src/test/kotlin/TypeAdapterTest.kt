e3a2b929372783ac638b7fc6a7e3d84b36a0ffde
Ahmed Ali <ahmedaliaay@gmail.com>
2023-10-19 20:52:32 +0300

    Add test for decoding all types of meta

--- sample/src/test/kotlin/TypeAdapterTest.kt
+++ sample/src/test/kotlin/TypeAdapterTest.kt
@@ -394,6 +394,41 @@ internal class TypeAdapterTest {
         )
     }
 
+    @org.junit.jupiter.api.Test
+    fun `given that a response that has multiple  all types of meta, should generate a Person with all types of meta`() {
+        val rootMeta = PersonRootMeta("root")
+        val resourceMeta = PersonResourceMeta("resource")
+        val relationship1Meta = PersonRelationshipMeta("relation1")
+        val relationship2Meta = PersonRelationshipMeta("relation2")
+
+        val person = Person(
+            name = "Jason",
+            surname = "Apix",
+            age = 28,
+            allMyDogs = listOf(Dog(name = "Bella", age = 1).apply { setId("1") }, Dog(name = "Bongo", age = 2).apply { setId("2") }),
+            myFavoriteDog = Dog(name = "Bella", age = 1),
+        )
+
+        val model = PersonModel(
+            data = person,
+            rootMeta = rootMeta,
+            resourceObjectMeta = resourceMeta,
+            relationshipsMeta = mapOf(
+                "myFavoriteDog" to relationship1Meta,
+                "allMyDogs" to relationship2Meta,
+            )
+        )
+
+        val response = getFileAsString("person_with_all_metas.json")
+
+        val result = typeAdapter?.convertFromString(response)
+
+        Assertions.assertEquals(
+            model.relationshipsMeta,
+            result?.relationshipsMeta
+        )
+    }
+
     private fun getFileAsString(filename: String): String {
         val fileStream = javaClass.classLoader?.getResourceAsStream(filename)
         val fileReader: InputStreamReader? = fileStream?.reader()
